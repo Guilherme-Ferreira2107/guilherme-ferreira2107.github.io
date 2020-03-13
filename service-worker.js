336292fc1f8ff4fb19ff92efbd980c8a83083018
Guilherme-Ferreira2107 <39266201+Guilherme-Ferreira2107@users.noreply.github.com>
2020-03-13 16:27:29 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4c2adc2a510f145560fece47c35d4e5.js"
+  "/precache-manifest.9a1c4c43c5d062bbe8646bc086893112.js"
 );
 
 self.addEventListener('message', (event) => {
