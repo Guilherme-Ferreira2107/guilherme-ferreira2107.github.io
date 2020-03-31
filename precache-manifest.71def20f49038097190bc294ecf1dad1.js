5d8357d3928162908a1d3be47bebaece9b87e85b
Guilherme Ferreira <guilhermeferreira2107@gmail.com>
2020-03-31 14:04:58 -0300

    Atualização Redux
    Redux atualizado em Tabelas

---
+++ precache-manifest.71def20f49038097190bc294ecf1dad1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a30a07654db5f3b6d9947962881141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7014c09d6f08c076fb12",
+    "url": "/static/css/main.adafd87b.chunk.css"
+  },
+  {
+    "revision": "e5a2fcc5f9524b1d4860",
+    "url": "/static/js/2.598f8620.chunk.js"
+  },
+  {
+    "revision": "63d48e2a12b3579e5e1e09dad89a6be2",
+    "url": "/static/js/2.598f8620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7014c09d6f08c076fb12",
+    "url": "/static/js/main.e285e9aa.chunk.js"
+  },
+  {
+    "revision": "e7629b7c6b39e2cab0bb",
+    "url": "/static/js/runtime-main.5a92c4c3.js"
+  },
+  {
+    "revision": "862b723557d9d1b4da3a52b4239be64d",
+    "url": "/static/media/logo.862b7235.png"
+  },
+  {
+    "revision": "de23cd38cb7eb3c2734862a533ba8cc5",
+    "url": "/static/media/logoC.de23cd38.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08eb315aa2e4d39ea8efd32e9345f279.js"
+  "/precache-manifest.71def20f49038097190bc294ecf1dad1.js"
 );
 
 self.addEventListener('message', (event) => {
